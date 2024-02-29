21a74b5775bc199f2aeb1d008fdbf173ad988576
PRdWit <143349091+PRdWit@users.noreply.github.com>
2024-02-29 13:41:17 +0100

    Fiddling around with switch

--- Database/commands/navigation.php
+++ Database/commands/navigation.php
@@ -5,6 +5,31 @@
          * move character over nodes to other rooms
          * the first argument is the direction (node name)
          */
-            echo "we all want to go {$arguments[0]} but it just doesn't work until you code it..\n";
+            $direction = $arguments;
+
+            switch($direction){
+                case 'east':
+                    echo "We're moving east! \n";
+                    break;
+                case 'north':
+                    echo "We're moving North \n";
+                    break;
+                case 'south':
+                    echo "We're moving South \n";
+                    break;
+                case 'west':
+                    echo "We're moving West \n";
+                    break;
+                default:
+                    echo "Where are we moving? \n";
+            };
+
+
+            /*if($arguments[0] == 'east'){
+                echo "Testing \n";
+            } else {
+                echo "Try a different direction";
+            }; */
+            //echo "we all want to go {$arguments[0]} but it just doesn't work until you code it..\n";
     }
 ?>
