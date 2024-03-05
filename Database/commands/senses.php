bf1760d63d4eaf851a00089e250a26bf65d20a8b
PRdWit <143349091+PRdWit@users.noreply.github.com>
2024-03-05 14:41:45 +0100

    Made look work somewhat

--- Database/commands/senses.php
+++ Database/commands/senses.php
@@ -4,9 +4,16 @@
       /**
        * describe what the character is looking at
        */
+         $current_room = "3";
 
       // echo "It \033[91mlooks\033[0m like you need to code me first....\n";
-         
+         $sql = "SELECT description FROM room WHERE node = :current_room";
+         $stmt = $conn->prepare($sql);
+         $stmt -> bindParam(':current_room', $current_room, PDO::FETCH_ASSOC);
+         $stmt->execute();
+         $room_description = $stmt->fetchColumn();
+
+         echo "You are in room {$current_room}. {$room_description}\n";
 
    }
 ?>
